19d00f1f01d39e2c2760a592238eab8136ffaaee
AllForOwl <sahavrysyuk@gmail.com>
2016-09-11 18:58:51 +0400

    delete unnecessary code from GameScene

--- GameScene.h
+++ GameScene.h
@@ -14,13 +14,7 @@ public:
 	static cocos2d::Scene *createScene();
 	virtual bool init();
 
-	void GameScene::AddHeroes(cocos2d::Ref* ref);
-
 	CREATE_FUNC(GameScene);
-	
-	void update(float dt);
-	void DrawSprite(float interval);
-	void UpdateHeroes(float dt);
 
 private:
 	cocos2d::PhysicsWorld* m_sceneWorld;
@@ -29,28 +23,10 @@ private:
 	{
 		m_sceneWorld = world;
 	}
-	bool OnContactBegin(cocos2d::PhysicsContact& contact);
 
-	void CallGraphicComponent();
-
-	Monster*	m_Heroes;
-	int			m_ID_HELL;
-	int			m_ID_FIRE_GIRL;
-	int			m_ID_WATER_MAN;
-	std::string m_listSprite[9];
-	int			m_count;
-	bool		m_createHeroes;
-
-	cocos2d::Sprite* m_spriteHeroes;
 	cocos2d::Sprite* m_background;
 
 	cocos2d::Layer* m_layer;
-
-	//Breed*				m_breed;
-	InputComponent*		inputComponent;
-	GraphicComponent*	graphicComponent;
-	Weapon*				weapon;
-
 };
 
 #endif // __GAME_SCENE_H__
